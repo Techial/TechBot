6b5b4fc2409fae4ab5152fdde7c7a8fa87002421
Techial <techial2@gmail.com>
2019-08-31 03:44:33 +0200

    Logged was accidentally excluded (Changed directory name)

---
+++ TechBot - Twitch Bot/Logger(Log)/Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechBot.Log
+{
+    static class Logger
+    {
+        public static void OutputToConsole(params string[] args)
+        {
+            string[] formats = new string[args.Length - 1];
+
+            int i = 0;
+            foreach(string f in args)
+            {
+                if (i>0)
+                {
+                    formats[i-1] = f;
+                }
+                i++;
+            }
+            string message = string.Format(args[0], formats);
+            Console.WriteLine(message);
+        }
+    }
+}
